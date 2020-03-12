b490dd5cdfdabfca3b19681b6d119a6e67ee3227
Robert Gray <rgray237@gmail.com>
2020-03-12 17:55:23 -0500

    Added dynamic blurb creation. Homepage gets info on blurbs from json file.

--- insertBlurbs.js
+++ insertBlurbs.js
@@ -1,48 +1,58 @@
-createABlurb(0);
 
-function createABlurb(id)
+
+let requestURL2 = "reviews.json";
+let request2 = new XMLHttpRequest();
+request2.open('GET', requestURL2);
+request2.responseType = 'json';
+request2.send();
+
+request2.onload = function() {
+  const response = request2.response;
+  const reviews = response['reviews'];
+  createABlurb(reviews,0);
+  createABlurb(reviews,1);
+  createABlurb(reviews,2);
+  createABlurb(reviews,3);
+  createABlurb(reviews,4);
+  document.getElementById("cover").style.height = "0%";
+}
+
+
+
+function createABlurb(reviews,id)
 {
-  let requestURL = "reviews.json";
-  let request = new XMLHttpRequest();
-  request.open('GET', requestURL);
-  request.responseType = 'json';
-  request.send();
-
-  request.onload = function() {
-    const reviews = request.response;
-    writeToBlurb(jsonObj,id);
-  }
 let blurb = document.createElement('div');
 blurb.className = "blurbSmall";
 let a = document.createElement('a');
-a.href = "nierautomata.html";
+a.href = reviews[id]["url"];
 //let img = document.create('img');
+let img = document.createElement('img');
+if (reviews[id]["rating"]=="dig")
+{
+  img.src = "dig.png";
+  img.className = "dig";
+}
+else
+{
+  img.src = "bury.png";
+  img.className = "bury";
+}
 let h1 = document.createElement('h1');
-h1.innerHTML="fuuuu";
 let txtFld = document.createElement('div');
 txtFld.className = "txtField";
 let p = document.createElement('p');
-p.innerHTML="bruuuuuu";
+
+
+
+p.innerHTML = reviews[id]["blurb"];
+h1.innerHTML = reviews[id]["title"];
 txtFld.appendChild(p);
+a.appendChild(img);
 a.appendChild(h1);
 a.appendChild(txtFld);
 
 
 blurb.appendChild(a);
-document.getElementById("blrbContainer").appendChild(blurb);
-}
-/*
-<div class="blurbSmall">
-  <a id="blrb1url">
-    <img src="dig.png" class="dig"></img>
-    <h1 id="blrb1title">f</h1>
-    <div class="txtField">
-      <p id="blrb1"> </p>
-    </div>
-  </a>
-</div>
-*/
-function writeToBlurb(jsonObj,id)
-{
 
+document.getElementById("blrbContainer").appendChild(blurb);
 }
